23651919c11baf499cf61dec07cadd53eaeff755
amsearles <asearles@mail.sfsu.edu>
2019-04-01 13:35:50 -0700

    Update twitter_bot_reddit.py

--- twitter_bot_reddit.py
+++ twitter_bot_reddit.py
@@ -4,11 +4,11 @@ import pandas as pd
 import datetime as dt
 from twitter import *
 #holds reddit data
-reddit = praw.Reddit(client_id='S-FNWZ8XEqEA6Q', \
-                     client_secret='XlxBCDr51d507sGur_V93faOymA', \
-                     user_agent='til_scraper', \
-                     username='sensitiverocketsfan', \
-                     password='akiena')
+reddit = praw.Reddit(client_id='', \
+                     client_secret='', \
+                     user_agent='', \
+                     username='', \
+                     password='')
 
 
 
@@ -17,10 +17,10 @@ subreddit = reddit.subreddit('NBA')
 
 #sorting by new and getting data from there
 new_subreddit = subreddit.hot(limit=25)
-token = '1111285762639618054-9dC3FSKRMmlGCjyJth75qxMRIDgcuv'
-token_secret = 'nUnfVi46s5nnGHaN5EXs9yaCVfw5w2AC03fsKrJduaDfN'
-consumer_key= 'onn48otrr2iOxyCXS98V5gTQL'
-consumer_secret= 'BJaXOhQ2OS2cNh7cnBDagAGxolYIKfjNvcAjeIdN2xUNtp4sxj'
+token = ''
+token_secret = ''
+consumer_key= ''
+consumer_secret= ''
 
 twit = Twitter(
     auth=OAuth(token, token_secret, consumer_key, consumer_secret))
