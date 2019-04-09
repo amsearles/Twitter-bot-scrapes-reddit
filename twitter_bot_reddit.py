ba6e0b802e78eae3a2d2f6061234efb6ae4fc135
amsearles <asearles@mail.sfsu.edu>
2019-04-09 13:39:02 -0700

    trying to shorten title urls for tweets

--- twitter_bot_reddit.py
+++ twitter_bot_reddit.py
@@ -1,7 +1,6 @@
 #! usr/bin/env python3
 import praw
-import pandas as pd
-import datetime as dt
+import time
 from twitter import *
 
 #Twitter Access Tokens
@@ -19,8 +18,7 @@ reddit = praw.Reddit(client_id='', \
 
 #Customization
 sub_name = "NBA"
-hash_tag_name = "#NBA"
-delay_of_tweets = 1
+delay_of_tweets = 10
  
 subreddit = reddit.subreddit(sub_name)
 new_subreddit = subreddit.hot(limit=25)
@@ -29,6 +27,11 @@ new_subreddit = subreddit.hot(limit=25)
 twit = Twitter(
     auth=OAuth(token, token_secret, consumer_key, consumer_secret))
 
-#loops through top 25 hot posts and posts the urls to twiter
+#loops through top 25 hot posts and posts the urls to twitter every x amount of seconds
 for new_submission in new_subreddit:
-  twit.statuses.update(status=new_submission.url)
+  if len(new_submission.url) < 140:
+    twit.statuses.update(status=new_submission.url)
+    time.sleep(delay_of_tweets)
+  else:
+    title = new_submission.url[:130] + "..."
+    twit.statuses.update(status=title)
